d2b7c9a631f0377a3ee8344c6603efc2a474ca83
Akash Kriplani <aakash23kriplani@gmail.com>
2023-02-18 21:50:49 +0530

    Dispatch sign in action from sign in screen to get token from mongo db

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { signin } from '../actions/userActions';
 
 export default function SigninScreen() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const dispatch = useDispatch();
+
   const submitHandler = (e) => {
     e.preventDefault();
-    // TODO: sign in action
+    dispatch(signin(email, password));
   };
 
   return (
