84ec99c92fa293f3dd6e5c1d26376ede576fdfaf
Akash Kriplani <aakash23kriplani@gmail.com>
2023-02-18 17:06:06 +0530

    Move get products API and product details API in productRouter

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -6,11 +6,31 @@ import Product from '../models/productModel.js';
 const productRouter = express.Router();
 
 productRouter.get(
+  '/',
+  expressAsyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.send(products);
+  })
+);
+
+productRouter.get(
   '/seed',
   expressAsyncHandler(async (req, res) => {
-    await Product.remove({});
+    // await Product.remove({});
     const createdProduct = await Product.insertMany(data.products);
     res.send({ createdProduct });
+  })
+);
+
+productRouter.get(
+  '/:id',
+  expressAsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) {
+      res.send(product);
+    } else {
+      res.status(404).send({ message: 'Product not found' });
+    }
   })
 );
 
