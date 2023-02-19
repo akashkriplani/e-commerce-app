f2ab162208f1a37b1bbfa2a0f91aeed97f77d60a
Akash Kriplani <aakash23kriplani@gmail.com>
2023-02-19 17:22:38 +0530

    ADD REGISTER SCREEN

---
+++ frontend/src/screens/RegisterScreen.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from 'react';
+import { Link, useSearchParams, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { register } from '../actions/userActions';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+
+export default function RegisterScreen() {
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const userRegister = useSelector((state) => state.userRegister);
+  const { userInfo, loading, error } = userRegister;
+
+  const [searchParams] = useSearchParams();
+  const redirectUrl = '/' + (searchParams.get('redirect') || '');
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(register(name, email, password));
+  };
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirectUrl);
+    }
+  }, [userInfo, redirectUrl, navigate]);
+
+  return (
+    <div>
+      <form className="form" onSubmit={submitHandler}>
+        <div>
+          <h1>Register</h1>
+        </div>
+        {loading && <LoadingBox />}
+        {error && <MessageBox variant="danger">{error}</MessageBox>}
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" placeholder="Enter name" required onChange={(e) => setName(e.target.value)} />
+        </div>
+        <div>
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <label></label>
+          <button className="primary" type="submit">
+            Register
+          </button>
+        </div>
+        <div>
+          <label></label>
+          <div>
+            Already have an account? <Link to="/signin">Sign In</Link>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
