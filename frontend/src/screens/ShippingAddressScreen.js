69ad5fc8febd99dadabca2998edae5904959e6b5
Akash Kriplani <aakash23kriplani@gmail.com>
2023-02-19 20:37:50 +0530

    Create a form in the shipping address screen

--- frontend/src/screens/ShippingAddressScreen.js
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -1,10 +1,107 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import CheckoutSteps from '../components/CheckoutSteps';
+import { saveShippingAddress } from '../actions/cartActions';
 
 export default function ShippingAddressScreen() {
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  const [fullName, setFullName] = useState(shippingAddress.fullName || '');
+  const [address, setAddress] = useState(shippingAddress.address || '');
+  const [city, setCity] = useState(shippingAddress.city || '');
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode || '');
+  const [country, setCountry] = useState(shippingAddress.country || '');
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+
+  useEffect(() => {
+    if (!userInfo) {
+      navigate('/signin');
+    }
+  }, [userInfo, navigate]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(saveShippingAddress({ fullName, address, city, postalCode, country }));
+    navigate('/payment');
+    // TODO: Dispatch save shipoping address action
+  };
+
   return (
     <div>
       <CheckoutSteps step1 step2></CheckoutSteps>
+      <form className="form" onSubmit={submitHandler}>
+        <div>
+          <h1>Shipping Address</h1>
+        </div>
+        <div>
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            type="text"
+            id="fullName"
+            placeholder="Enter full name"
+            value={fullName}
+            required
+            onChange={(e) => setFullName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="address">Address</label>
+          <input
+            type="text"
+            id="address"
+            placeholder="Enter address"
+            value={address}
+            required
+            onChange={(e) => setAddress(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="city">City</label>
+          <input
+            type="text"
+            id="city"
+            placeholder="Enter city"
+            value={city}
+            required
+            onChange={(e) => setCity(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="postalCode">Postal Code</label>
+          <input
+            type="text"
+            id="postalCode"
+            placeholder="Enter postal code"
+            value={postalCode}
+            required
+            onChange={(e) => setPostalCode(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="country">Country</label>
+          <input
+            type="text"
+            id="country"
+            placeholder="Enter country"
+            value={country}
+            required
+            onChange={(e) => setCountry(e.target.value)}
+          />
+        </div>
+        <div>
+          <label></label>
+          <button className="primary" type="submit">
+            Continue
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
