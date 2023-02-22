dbc5747fd1d9a719b75b4d5d856dc41441a7dec6
Akash Kriplani <aakash23kriplani@gmail.com>
2023-02-22 21:38:03 +0530

    Make the product screen route exact

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -91,7 +91,7 @@ function App() {
         <main>
           <Routes>
             <Route path="/" element={<HomeScreen />} exact />
-            <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path="/product/:id" exact element={<ProductScreen />} />
             <Route path="/cart/:id?" element={<CartScreen />} />
             <Route path="/signin" element={<SigninScreen />} />
             <Route path="/register" element={<RegisterScreen />} />
