9f0797eba04efe8d596ef866da1e6b19308b6cd1
Akash Kriplani <aakash23kriplani@gmail.com>
2023-02-22 21:36:43 +0530

    Add productcreate reducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,8 @@
 import {
+  PRODUCT_CREATE_FAILURE,
+  PRODUCT_CREATE_REQUEST,
+  PRODUCT_CREATE_RESET,
+  PRODUCT_CREATE_SUCCESS,
   PRODUCT_DETAILS_FAILURE,
   PRODUCT_DETAILS_REQUEST,
   PRODUCT_DETAILS_SUCCESS,
@@ -28,6 +32,21 @@ export const productDetailsReducer = (state = { loading: true, product: {} }, ac
       return { loading: false, product: action.payload };
     case PRODUCT_DETAILS_FAILURE:
       return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_CREATE_REQUEST:
+      return { loading: true };
+    case PRODUCT_CREATE_SUCCESS:
+      return { loading: false, success: true, product: action.payload };
+    case PRODUCT_CREATE_FAILURE:
+      return { loading: false, error: action.payload };
+    case PRODUCT_CREATE_RESET:
+      return {};
     default:
       return state;
   }
