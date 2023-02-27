48799ce6e665938b8a5b9e22fd90dd2d4a4da70c
Akash Kriplani <aakash23kriplani@gmail.com>
2023-02-27 21:52:48 +0530

    Create delete API to delete an order from the order list route

--- backend/routers/orderRouter.js
+++ backend/routers/orderRouter.js
@@ -84,4 +84,19 @@ orderRouter.put(
   })
 );
 
+orderRouter.delete(
+  '/:id',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if (order) {
+      const deleteOrder = await order.remove();
+      res.send({ message: 'Order Deleted', order: deleteOrder });
+    } else {
+      res.status(404).send({ message: 'Order not found' });
+    }
+  })
+);
+
 export default orderRouter;
