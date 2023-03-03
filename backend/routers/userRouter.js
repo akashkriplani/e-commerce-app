48b8864aa280c308213fbc408535397a518768a4
Akash Kriplani <aakash23kriplani@gmail.com>
2023-03-03 14:27:13 +0530

    Add update user API

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -122,4 +122,23 @@ userRouter.delete(
   })
 );
 
+userRouter.put(
+  '/:id',
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      user.name = req.body.name || user.name;
+      user.email = req.body.email || user.email;
+      user.isSeller = req.body.isSeller || user.isSeller;
+      user.isAdmin = req.body.isAdmin || user.isAdmin;
+      const updateUser = await user.save();
+      res.send({ message: 'User updated', user: updateUser });
+    } else {
+      res.status(404).send({ message: 'User not found' });
+    }
+  })
+);
+
 export default userRouter;
